95f63b03be37e24ee19b883e4fae667524455b38
berna_cabrera@hotmail.com <berna_cabrera@hotmail.com>
2020-10-20 16:54:45 -0500

    Tarea para entregar el martes

--- Testcafe/pageModel.js
+++ Testcafe/pageModel.js
@@ -4,12 +4,13 @@ class Page {
     constructor () {
     // Practica 2
     this.link2 = Selector('a').withText('A/B Testing');
-    this.text21 = Selector ('#content > div > h3');
+    this.text21 = Selector ('#content > div > h3'); 
 
     //Practica 3
     this.link3 = Selector('a').withText('Forgot Password');
     this.input31 = Selector('#email');
     this.button32 =  Selector('form_submit');
+    this.forgot = Selector('#form_submit > i')
     this.text33 = Selector('#content');
 
     //Practica 4
--- Testcafe/practica2.js
+++ Testcafe/practica2.js
@@ -4,6 +4,16 @@ import page from './pageModel';
 fixture('practica 2 con testcafe')
     .page('https://the-internet.herokuapp.com/');
 
-    test('Probar click en un enlace', async t => {
+test('Probar click en el enlace', async t => {
+    await t
+        .click(page.link2)
 
+    await t
+    if (page.text21.innerText == "Test"){
+        console.log ("Pasa")
+    } else {
+        console.log("No pasa")
+        
+    }
     });
+    
