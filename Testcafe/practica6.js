796316686f64d82feaf4ef9ab64728bf7d6eb5e7
Bernardo Cabrera <63157549+bernacab@users.noreply.github.com>
2020-10-22 21:54:31 -0500

    Update practica6.js

--- Testcafe/practica6.js
+++ Testcafe/practica6.js
@@ -1,38 +1,24 @@
-import { Selector } from 'testcafe';
+import page from './pageModel';
 
-class Page {
-    constructor () {
-    // Practica 2
-    this.link2 = Selector('a').withText('A/B Testing');
-    //this.text21 = Selector ('#content > div > h3');
-    this.text21 = Selector ('h3').innerText;
-    //const abText = Selector('h3')
+fixture('Test con testcafe')
+    .page('https://the-internet.herokuapp.com/');
 
-    // Practica 3
-    this.link3 = Selector('a').withText('Forgot Password');
-    this.input31 = Selector('#email'); 
-    this.button32 = Selector('#form_submit');
-    this.text33 = Selector('#content');
+test('Probar agregar y quitar botones', async t =>{
+    await t
+        .click(page.link6)
+    
+    await t
+        .expect(page.removeBtn62.visible).notOk()
+        .click(page.addBtn61)
+        .click(page.addBtn61)
+        .click(page.addBtn61)
+        .click(page.addBtn61)
+        
+        .expect(page.removeBtn62.visible).ok()
+        .wait(3000)
 
-    // Practica 4
-    this.link4 = Selector('a').withText('Checkboxes');
-    //Checkbox Padre
-    this.baseCheckbox = Selector('#checkboxes > input[type=checkbox]')
-
-    //Checboxs Hijos
-    //this.checkbox41 = Selector('#checkboxes > input[type=checkbox]:nth-child(1)');
-    this.checkbox42 = Selector('#checkboxes > input[type=checkbox]:nth-child(3)');
-
-    //Practica 5
-    this.link5 = Selector('a').withText('Inputs');
-    this.input51 = Selector('#content > div > div > div > input[type=number]');
-
-    //Practica 6
-    this.link6 = Selector('a').withText('Add/Remove Elements');
-    this.addBtn61 = Selector('#content > div > button')
-    this.removeBtn62 = Selector('#elements > button')
-    this.removeBtn63 = Selector('#elements > button:nth-child(2)')
-    this.removeBtn64 = Selector('#elements > button:nth-child(3)')
-    }
-}
-export default new Page();
+        .click(page.removeBtn62)
+        .click(page.removeBtn63)
+        .click(page.removeBtn64)
+        .expect(page.removeBtn62.visible).notOk()
+});
